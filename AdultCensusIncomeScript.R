d63448c11a5901d6b454a68176e83667a1893144
Pim Kempe <pdkempe@gmail.com>
2020-03-14 10:51:26 +0100

    Update Script

--- AdultCensusIncomeScript.R
+++ AdultCensusIncomeScript.R
@@ -1,6 +1,11 @@
 # Install and load packages
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(tibble)) install.packages("tibble", repos = "http://cran.us.r-project.org")
+if(!require(scales)) install.packages("scales", repos = "http://cran.us.r-project.org")
+if(!require(forcats)) install.packages("forcats", repos = "http://cran.us.r-project.org")
 if(!require(dslabs)) install.packages("dslabs", repos = "http://cran.us.r-project.org")
+
 
 # Set ggplot theme
 ds_theme_set()
@@ -20,36 +25,12 @@ clean_data <- clean_data %>%
 str(clean_data)
 head(clean_data)
 
-clean_data %>% 
-  group_by(workclass, income) %>% 
-  summarize(n=n()) %>% 
-  ggplot(aes(x=workclass, y=n, fill=income)) +
-    geom_bar(position="fill", stat="identity") +
-    scale_y_continuous(labels = scales::percent) +
-    scale_fill_discrete(name = "Income", labels = c("<=50K", ">50K")) +
-    coord_flip()
 
 clean_data %>% 
-  filter(!is.na(workclass)) %>% 
-  group_by(workclass, income) %>% 
-  summarize(n=n()) %>% 
-  mutate(freq = n / sum(n)) %>%
-  spread(income,n) %>% 
-  mutate(`0`= ifelse(is.na(`0`), 0, `0`), `1`= ifelse(is.na(`1`), 0, `1`)) %>% 
-  gather(key=income, value = freq, 2:3) %>% 
-  ggplot(aes(x=reorder(workclass, -n), y=freq, fill=income)) +
-    geom_bar(position="fill", stat="identity") +
-    scale_y_continuous(labels = scales::percent) +
-    scale_fill_discrete(name = "Income", labels = c("<=50K", ">50K")) +
-    coord_flip()
-
-df = df %>% mutate_if(is.factor,
-                      fct_explicit_na,
-                      na_level = "to_impute")
-
-clean_data2 <- clean_data %>% 
-  
-str(clean_data2)
-levels(clean_data2$workclass)
-table(clean_data$workclass)
-table(clean_data2$workclass)
+  select(workclass, income) %>% 
+  mutate(workclass = fct_reorder(workclass, as.numeric(income),mean)) %>% 
+  ggplot(aes(x = workclass, fill = income)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = percent) +
+  scale_fill_discrete(name = "Income", labels = c("<=50K", ">50K")) +
+  coord_flip()
